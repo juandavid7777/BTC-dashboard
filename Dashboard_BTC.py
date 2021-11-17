a9254d97f8713830924279f2df5cb5a4e9fc562d
juandavid7777 <71436816+juandavid7777@users.noreply.github.com>
2021-11-17 13:05:47 +0400

    Added plotly graphs

--- Dashboard_BTC.py
+++ Dashboard_BTC.py
@@ -2,71 +2,101 @@
 """
 Created on Wed Nov 17 09:19:21 2021
 
-@author: edwin
+@author: Juan
 """
 
 import streamlit as st
+import plotly.graph_objects as go
 import pandas as pd
-import numpy as np
-from matplotlib import pyplot as plt
+from datetime import datetime
 
 
-df_projected = pd.read_csv("https://raw.githubusercontent.com/juandavid7777/BTC_risk_metric/891a1032cb63160763c33f235a4714f2083c9713/BTC_price_projections.csv")
+#1.-----Downloads data
+df = pd.read_csv("https://raw.githubusercontent.com/juandavid7777/BTC_risk_metric/891a1032cb63160763c33f235a4714f2083c9713/BTC_price_projections.csv")
 
 
-
-st.title("""#BTC main Dashboard""")
-
-st.line_chart(df_projected[["close"]])
-
-#1.---API token definition
+#2.-----API token definition
 coin_name = "BTC"
 projected_days = 180
 
 
-#2.---Defines plotting parameters
-#Background color
-plt.style.use('dark_background')
+#3.-----Plots figures
 
-#Plot size
-plt.rcParams['figure.figsize'] = [16, 8]
+fig = go.Figure()
 
-#Price boundaries to plot
-max_price_plot = 100000 
-min_price_plot = 0.1
+#Price candlesticks plots
+fig.add_trace(go.Candlestick(
+    x=df['Date'],
+    open=df['open'],
+    high=df['high'],
+    low=df['low'],
+    close=df['close'],
+    name = coin_name + ' price'
+    ))
 
+#Prices for uncertainity bands
+fig.add_trace(go.Scatter(
+    x=df['Date'],
+    y=df["plus_3STDV"],
+    mode = 'lines',
+    name = '99.9%',
+    line = dict(width = 1.5, dash = 'dash', color = "red")
+    ))
 
-#3.---Plots regresion bands
-fig, ax1 = plt.subplots()
-plt.ylim(min_price_plot,max_price_plot*10)
+fig.add_trace(go.Scatter(
+    x=df['Date'],
+    y=df["plus_2STDV"],
+    mode = 'lines',
+    name = '97.8%',
+    line = dict(width = 1.5, dash = 'dash', color = "yellow")
+    ))
 
-ax1.set_title(coin_name + " prediction bands")
-ax1.set_ylabel(coin_name + " price (USD)")
+fig.add_trace(go.Scatter(
+    x=df['Date'],
+    y=df["plus_1STDV"],
+    mode = 'lines',
+    name = '84.2%',
+    line = dict(width = 1.5, dash = 'dash', color = "green")
+    ))
 
-#Defines if logarithmic graph
-log_val = True
+#Prices regression plot
+fig.add_trace(go.Scatter(
+    x=df['Date'],
+    y=df["price_reg"],
+    mode = 'lines',
+    name = '50.0%',
+    line = dict(width = 1.5, dash = 'dash', color = "grey")
+    ))
 
-#Plots regression bands
-df_projected.plot(kind = "line", y = "plus_3STDV", logy=log_val, ax = ax1, secondary_y = False, c = "red", label="99.9%")
-df_projected.plot(kind = "line", y = "plus_2STDV", logy=log_val, ax = ax1, secondary_y = False, c = "yellow", label="97.8%")
-df_projected.plot(kind = "line", y = "plus_1STDV", logy=log_val, ax = ax1, secondary_y = False, c = "green", label="84.2%")
-df_projected.plot(kind = "line", y = "price_reg", logy=log_val, ax = ax1, secondary_y = False, c = "grey", linestyle='dashed', label="50.0%")
-df_projected.plot(kind = "line", y = "minus_1STDV", logy=log_val, ax = ax1, secondary_y = False, c = "green", label="15.8%")
-df_projected.plot(kind = "line", y = "minus_2STDV", logy=log_val, ax = ax1, secondary_y = False, c = "yellow", label="2.2%")
-df_projected.plot(kind = "line", y = "minus_3STDV", logy=log_val, ax = ax1, secondary_y = False, c = "red", label="0.1%")
+fig.add_trace(go.Scatter(
+    x=df['Date'],
+    y=df["minus_1STDV"],
+    mode = 'lines',
+    name = '15.8%',
+    line = dict(width = 1.5, dash = 'dash', color = "green")
+    ))
 
-#color bands
-ax1.fill_between(df_projected.index, y1 = df_projected["plus_3STDV"], y2 = df_projected["plus_2STDV"], color='r', alpha=.2)
-ax1.fill_between(df_projected.index, y1 = df_projected["plus_2STDV"], y2 = df_projected["plus_1STDV"], color='yellow', alpha=.2)
-ax1.fill_between(df_projected.index, y1 = df_projected["minus_1STDV"], y2 = df_projected["plus_1STDV"], color='g', alpha=.2)
-ax1.fill_between(df_projected.index, y1 = df_projected["minus_3STDV"], y2 = df_projected["minus_2STDV"], color='r', alpha=.2)
-ax1.fill_between(df_projected.index, y1 = df_projected["minus_2STDV"], y2 = df_projected["minus_1STDV"], color='yellow', alpha=.2)
+fig.add_trace(go.Scatter(
+    x=df['Date'],
+    y=df["minus_2STDV"],
+    mode = 'lines',
+    name = '2.2%',
+    line = dict(width = 1.5, dash = 'dash', color = "yellow")
+    ))
 
-#Plots closing price
-df_projected.plot(kind = "line", y = "close", logy=log_val, ax = ax1, secondary_y = False, c = "white", label= coin_name + " price")
+fig.add_trace(go.Scatter(
+    x=df['Date'],
+    y=df["minus_3STDV"],
+    mode = 'lines',
+    name = '0.1%',
+    line = dict(width = 1.5, dash = 'dash', color = "red")
+    ))
 
-#Creates grid
-plt.grid(b=True, which = 'both', axis = 'both', alpha = 0.3)
+#Defines the y axis log scale
+fig.update_layout(
+    title = coin_name + " uncertainity bands",
+    plot_bgcolor = "black",
+    yaxis_type="log",
+    xaxis_rangeslider_visible=False)
 
-
-st.pyplot(fig)
+st.plotly_chart(fig)
